663f4c7e244df5810fd71879aa072727f7f92b0d
novak-fei-stuba <vladislav.novak@stuba.sk>
2025-02-26 10:30:50 +0100

    Prva verzia tlace obdlznika

---
+++ src/Rectangle.java
@@ -0,0 +1,36 @@
+public class Rectangle {
+    public static void print(int width, int height) {
+        // https://www.w3.org/TR/xml-entity-names/025.html
+        final char TOP_LEFT = '\u250C';
+        final char TOP_RIGHT = '\u2510';
+        final char HORIZONTAL = '\u2500';
+        final char VERTICAL = '\u2502';
+        final char BOTTOM_LEFF = '\u2514';
+        final char BOTTOM_RIGHT = '\u2518';
+//        final char  = '';
+
+        System.out.print(TOP_LEFT);
+        for (int i = 0; i < width - 2; i ++) {
+            System.out.print(HORIZONTAL);
+        }
+        System.out.println(TOP_RIGHT);
+
+        for (int i = 0; i < height - 2; i ++) {
+            System.out.print(VERTICAL);
+            for (int j = 0; j < width - 2; j ++) {
+                System.out.print(" ");
+            }
+            System.out.println(VERTICAL);
+        }
+        System.out.print(BOTTOM_LEFF);
+        for (int i = 0; i < width - 2; i ++) {
+            System.out.print(HORIZONTAL);
+        }
+        System.out.println(BOTTOM_RIGHT);
+
+
+
+
+
+    }
+}
---
+++ src/RectangleTester.java
@@ -0,0 +1,5 @@
+public class RectangleTester {
+    public static void main(String[] args) {
+        Rectangle.print(10, 5);
+    }
+}
