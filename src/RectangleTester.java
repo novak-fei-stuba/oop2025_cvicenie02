cf0c4e58a8bc757e91e140ac73e24ffa45af2bae
novak-fei-stuba <vladislav.novak@stuba.sk>
2025-02-26 11:17:09 +0100

    Pridaj testy pre kreslenie obdlznika

--- src/RectangleTester.java
+++ src/RectangleTester.java
@@ -2,5 +2,9 @@ public class RectangleTester {
     public static void main(String[] args) {
         Rectangle.print(10, 5);
         Rectangle.print(0, 0);
+        Rectangle.print(1, 5);
+        Rectangle.print(2, 5);
+        Rectangle.print(10, 1);
+        Rectangle.print(10, 2);
     }
 }
